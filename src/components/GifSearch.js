bda3362717226df1b1cfb38443a4e7ca6507de62
github username <github email address>
2019-02-22 15:45:16 -0500

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class GifSearch extends Component {
 
     render() {
-        return (null)
+        return (
+            <div>
+            <form onSubmit={this.handleSubmit}>
+            <label>
+            Search:
+            <textarea value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+            </form>
+            </div>
+        )
     }
 }
 
